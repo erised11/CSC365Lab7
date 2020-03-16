972110f137b8d8a37731616d232bd7b63b745b52
Nick Ryan <25760277+nickryan002@users.noreply.github.com>
2020-03-16 14:40:28 -0700

    FR 5 complete

--- Lab7/src/InnReservations.java
+++ Lab7/src/InnReservations.java
@@ -49,6 +49,7 @@ public class InnReservations {
                     break;
                 case "5":
                     System.out.println("Detailed Reservation Information:\n");
+                    SearchReservation(scanner);
                     break;
                 case "0":
                     return;
@@ -114,6 +115,150 @@ public class InnReservations {
                 conn.commit();
 
             } catch (SQLException e){
+                conn.rollback();
+            }
+
+        } catch (SQLException e){
+            System.out.println("Connection couldn't be made with database");
+        }
+
+    }
+
+    private void SearchReservation(Scanner scanner) {
+
+        // Step 1: Establish connection to RDBMS
+        try (Connection conn = DriverManager.getConnection(System.getenv("HP_JDBC_URL"),
+                System.getenv("HP_JDBC_USER"),
+                System.getenv("HP_JDBC_PW"))) {
+
+            // Step 2: Construct sql statement
+            String firstName;
+            String lastName;
+            String checkIn;
+            String checkOut;
+            String roomCode;
+            String resCode;
+
+            System.out.println("Search with: ");
+            System.out.print("First name: ");
+            firstName = scanner.nextLine();
+
+            System.out.print("Last name: ");
+            lastName = scanner.nextLine();
+
+            System.out.print("Check in date: ");
+            checkIn = scanner.nextLine();
+
+            System.out.print("Check out date: ");
+            checkOut = scanner.nextLine();
+
+            System.out.print("Room Code: ");
+            roomCode = scanner.nextLine();
+
+            System.out.print("Reservation Code: ");
+            resCode = scanner.nextLine();
+
+            String sqlStatement = "SELECT * from lab7_reservations where ";
+
+            if (firstName.equals("")) {
+                sqlStatement += "firstname is not null ";
+            }
+            else
+                sqlStatement += "firstname = ? ";
+
+            if (lastName.equals("")) {
+                sqlStatement += "and lastname is not null ";
+            }
+            else
+                sqlStatement += "and lastname = ? ";
+
+
+            if (checkIn.equals("")) {
+                sqlStatement += "and checkIn is not null ";
+            }
+            else
+                sqlStatement += "and checkIn = ? ";
+
+            if (checkOut.equals("")) {
+                sqlStatement += "and checkOut is not null ";
+            }
+            else
+                sqlStatement += "and checkOut = ? ";
+
+            if (roomCode.equals("")) {
+                sqlStatement += "and room is not null ";
+            }
+            else
+                sqlStatement += "and room = ? ";
+
+            if (resCode.equals("")) {
+                sqlStatement += "and code is not null ";
+            }
+            else
+                sqlStatement += "and code = ? ";
+
+            // Step 3: start transaction
+            conn.setAutoCommit(false);
+
+            try (PreparedStatement pstmt = conn.prepareStatement(sqlStatement)) {
+
+                System.out.println("***");
+                // Step 4: Send SQL statement to DBMS
+                int numParams = 0;
+                if (!firstName.equals("")) {
+                    numParams++;
+                    pstmt.setString(numParams, firstName);
+                }
+
+                if (!lastName.equals("")) {
+                    numParams++;
+                    pstmt.setString(numParams, lastName);
+                }
+
+                if (!checkIn.equals("")) {
+                    numParams++;
+                    pstmt.setString(numParams, checkIn);
+                }
+
+                if (!checkOut.equals("")) {
+                    numParams++;
+                    pstmt.setString(numParams, checkOut);
+                }
+
+                if (!roomCode.equals("")) {
+                    numParams++;
+                    pstmt.setString(numParams, roomCode);
+                }
+
+                if (!resCode.equals("")) {
+                    numParams++;
+                    pstmt.setString(numParams, resCode);
+                }
+
+                System.out.println("***");
+                ResultSet rs = pstmt.executeQuery();
+
+                System.out.println("sql: " + sqlStatement);
+
+                while(rs.next()) {
+                    System.out.print("Reservation Code: " + rs.getInt("code") + ", ");
+                    System.out.print("Room Code: " + rs.getString("room") + ", ");
+                    System.out.print("Nightly Rate: " + rs.getString("rate") + ", ");
+                    System.out.print("Last Name: " + rs.getString("lastname") + ", ");
+                    System.out.print("First Name: " + rs.getString("firstname") + ", ");
+                    System.out.print("Adults: " + rs.getInt("adults") + ", ");
+                    System.out.print("Kids: " + rs.getInt("kids") + ", ");
+                    System.out.print("Check in Date: " + rs.getString("checkIn") + ", ");
+                    System.out.println("Check out Date: " + rs.getString("checkOut"));
+                }
+
+
+                // Step 5: Handle results
+
+                conn.commit();
+
+            } catch (SQLException e){
+                System.out.println("Rollback" + e);
                 conn.rollback();
             }
 
